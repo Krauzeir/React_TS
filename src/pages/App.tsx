2c844348448b9b04d0bf951877a6953ae2ab1a26
Krauzeir <brunogox9@gmail.com>
2022-06-26 22:48:28 -0300

    React/TypeScript Lista de Tarefas

---
+++ src/pages/App.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import Button from '../components/Button';
+import Cronometro from '../components/Cronometro';
+import Form from '../components/Form';
+import Lista from '../components/Lista';
+import { ITarefa } from '../types/tarefa';
+import style from './App.module.scss';
+
+function App() {
+  const [tarefas, setTarefas] = useState<ITarefa[]>([])
+  const[selecionado, setSelecionado] = useState<ITarefa>()
+
+  function selecionaTarefa(tarefaSelecionada: ITarefa) {
+    setSelecionado(tarefaSelecionada);
+    setTarefas(tarefasAnteriores => tarefasAnteriores.map(tarefa => ({
+      ...tarefa,
+      selecionado: tarefa.id === tarefaSelecionada.id ? true : false
+    })));
+
+  }
+
+  function finalizarTarefa() {
+    if(selecionado) {
+        setSelecionado(undefined)
+        setTarefas(tarefasAnteriores =>
+        tarefasAnteriores.map(tarefa => {
+            if(tarefa.id === selecionado.id) {  
+                return {
+                    ...tarefa,
+                    selecionado: false,
+                    completado: true
+                }
+            }
+            return tarefa;
+        }))
+    }
+}
+
+  return (
+    <div className={style.AppStyle}>
+        <Form setTarefas={setTarefas}/>
+        <Lista 
+          tarefas={tarefas}
+          selecionaTarefa={selecionaTarefa}
+        />
+        <Cronometro 
+          selecionado={selecionado}
+          finalizarTarefa={finalizarTarefa}
+        />
+    </div>
+  );
+}
+
+export default App;
